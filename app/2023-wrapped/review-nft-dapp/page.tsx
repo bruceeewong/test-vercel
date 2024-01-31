a075cacbee89ec98ecd3a0667c954736a9f62fa8
brrruski <bruski@suiet.app>
2024-01-31 15:03:16 -0500

    feat(2023-wrapped): add accountButton for nft&dapp page

--- app/2023-wrapped/review-nft-dapp/page.tsx
+++ app/2023-wrapped/review-nft-dapp/page.tsx
@@ -13,6 +13,7 @@ import {
 import { Routes } from "@/configs/routes";
 import clsx from "clsx";
 import { Statistics } from "@/app/2023-wrapped/review-nft-dapp/components/Statistics";
+import { AccountButton } from "@/components/wallet-kit/AccountButton";
 
 type ReviewNftDappPageProps = {};
 
@@ -29,8 +30,16 @@ export default function ReviewNftDappPage(props: ReviewNftDappPageProps) {
       )}
     >
       <SoftCard
-        className={clsx("flex-1", "md:w-[1200px] md:h-[660px] md:flex-none")}
+        className={clsx(
+          "relative flex-1",
+          "md:w-[1200px] md:h-[660px] md:flex-none",
+        )}
       >
+        <AccountButton
+          className={
+            "absolute -top-4 left-[50%] transform -translate-x-[50%] w-[200px]"
+          }
+        />
         <div
           className={clsx(
             "flex flex-col justify-center w-[100vw] py-12 px-10",
@@ -41,7 +50,7 @@ export default function ReviewNftDappPage(props: ReviewNftDappPageProps) {
           <Statistics />
         </div>
       </SoftCard>
-      <footer className={clsx("mt-16 mb-12", "md:mt-[40px] md:mb-0")}>
+      <footer className={clsx("mt-16 mb-12", "md:mt-4 md:mb-0")}>
         <Toolbar className={clsx("justify-end gap-4", "md:w-[1200px]")}>
           <ToolbarPrevButton url={Routes.WRAP23__REVIEW_TXN} />
           <div className={"flex items-center gap-4"}>
