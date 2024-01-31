ad428763e9d8a5251732cf2714a3da502a2e0fb6
brrruski <bruski@suiet.app>
2024-01-31 11:45:28 -0500

    feat(2023-wrapped): add connection guard

--- components/buttons/CustomConnectButton.tsx
+++ components/buttons/CustomConnectButton.tsx
@@ -5,31 +5,37 @@ import { Extendable } from "@/types/global";
 import { useCallback, useEffect } from "react";
 import { Routes } from "@/configs/routes";
 import { useRouter } from "next/navigation";
+import { Button, message } from "antd";
 
 export type ConnectButtonProps = Extendable & {};
 
 export const CustomConnectButton = (props: ConnectButtonProps) => {
-  const { name } = useWallet();
+  const [messageApi, contextHolder] = message.useMessage();
+
+  const { name, disconnect } = useWallet();
   const router = useRouter();
 
   const handleConnectSuccess = useCallback(
-    (name: string) => {
-      if (name) {
-        router.push(Routes.WRAP23__REVIEW_TXN);
+    (name: string | undefined) => {
+      if (!name) return;
+      if (name !== "Suiet") {
+        messageApi.info("Please use Suiet wallet to connect.");
+        disconnect();
+        return;
       }
+      router.push(Routes.WRAP23__REVIEW_TXN);
     },
     [router],
   );
 
   useEffect(() => {
-    if (!name) return;
-
-    // TODO: check if wallet is Suiet?
-    // console.log("connected wallet name", name);
-    // if (name !== "Suiet") return;
-
     handleConnectSuccess(name);
   }, [name]);
 
-  return <ConnectButton {...props} onConnectSuccess={handleConnectSuccess} />;
+  return (
+    <div>
+      {contextHolder}
+      <ConnectButton {...props} onConnectSuccess={handleConnectSuccess} />
+    </div>
+  );
 };
